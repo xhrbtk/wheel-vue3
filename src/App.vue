0b1a8ae3ceeca600d8687e78c4a42c428372c847
xu.hongrui <xu.hongrui@uneed.com>
2022-01-05 17:03:44 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <Frank></Frank>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
