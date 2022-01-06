3291ad9ba6c435439f79c5e30bcc853918b0f278
xu.hongrui <xu.hongrui@uneed.com>
2022-01-06 11:13:48 +0800

    feat:添加doc样式并调整aside和topnav位置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>wheel-ui</h1>
             <h2>一个厉害的ui框架</h2>
             <p class="actions">
-                <a href="">github</a>
-                <a href="">开始</a>
+                <a href="https://github.com/xhrbtk/wheel-vue3">github</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
