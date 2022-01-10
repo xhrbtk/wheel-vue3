750296eb450bb8776cc3d69f822b4fd36a39c399
xu.hongrui <xu.hongrui@uneed.com>
2022-01-10 17:36:42 +0800

    fix:基本完善button

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="wheel-button" :class="classes">
+    <button class="wheel-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="wheel-loadingIndicator"></span>
         <slot />
     </button>
 </template>
@@ -15,27 +16,42 @@ export default{
        size: {
            type: String,
            default: "normal"
-       }
+       },
+       level: {
+            type: String,
+            default: "normal",
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        }
    },
    setup(props){
-       const { theme, size } = props
-       console.log(theme, size)
+       const { theme, size,  level} = props
+       console.log(theme, size, level)
        const classes = computed(() => {
            return { 
                [`wheel-theme-${theme}`]: theme, 
-               [`wheel-size-${size}`]: size
+               [`wheel-size-${size}`]: size,
+               [`wheel-level-${level}`]: level
             }
        })
        return { classes }
    }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .wheel-button {
   box-sizing: border-box;
   height: $h;
@@ -50,8 +66,8 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
-    //   wheel-button + wheel-button
     margin-left: 8px;
   }
   &:hover,
@@ -65,33 +81,110 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-  &.wheel-theme-link{
-      border-color: transparent;
-      box-shadow: none;
+  &.wheel-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.wheel-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.wheel-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.wheel-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.wheel-theme-button {
+    &.wheel-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.wheel-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.wheel-theme-link {
+    &.wheel-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.wheel-theme-text {
+    &.wheel-level-main {
       color: $blue;
-      &:hover,&:focus{
-          color: lighten($blue, 10%);
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
       }
+    }
+    &.wheel-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
-  &.wheel-theme-text{
-      border-color: transparent;
-      box-shadow: none;
-      color: inherit;
-      &:hover, &:focus{
-          background: darken(white, 5%);
+  &.wheel-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
       }
+    }
   }
-  &.wheel-theme-button{
-      &.wheel-size-big{
-        font-size: 24px;
-        height: 48px;
-        padding: 0 16px
-      }
-      &.wheel-size-small{
-        font-size: 12px;
-        height: 20px;
-        padding: 0 4px;
-      }
+  &.wheel-theme-link, &.wheel-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
   }
+  > .wheel-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: wheel-spin 1s infinite linear;
+  }
+}
+@keyframes wheel-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>
