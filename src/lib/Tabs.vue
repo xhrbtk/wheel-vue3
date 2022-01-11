4b0a518f186d789bad6719b8909ef55900c09bf8
xu.hongrui <xu.hongrui@uneed.com>
2022-01-11 19:01:21 +0800

    feat:添加css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-        <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
+    <div class="wheel-tabs">
+        <div class="wheel-tabs-nav">
+            <div class="wheel-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="wheel-tabs-content">
+            <component class="wheel-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+        </div>
     </div>
 </template>
 
@@ -21,3 +25,29 @@ export default{
     }
 }
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
