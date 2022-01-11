f07849fa107f89dcb07e55ed2be1aff8a66e3398
xu.hongrui <xu.hongrui@uneed.com>
2022-01-11 12:19:41 +0800

    feat:添加visibleshuxign

--- src/components/Dialogdemo.vue
+++ src/components/Dialogdemo.vue
@@ -1,8 +1,16 @@
 <template>
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="visible"></Dialog>
 </template>
 
 <script lang="ts" setup>
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue';
+const visible = ref(false)
+const toggle = () => {
+    console.log('lllll')
+    visible.value = !visible.value
+}
 </script>
