a0645c45561f094861eadbdd4576c124162ea922
xu.hongrui <xu.hongrui@uneed.com>
2022-01-11 16:38:20 +0800

    feat:创建tabs 和 tab 组件

--- src/components/Tabsdemo.vue
+++ src/components/Tabsdemo.vue
@@ -1,3 +1,12 @@
 <template>
     <div>Tabsdemo 文档</div>
+    <Tabs>
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs>
 </template>
+
+<script lang="ts" setup>
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+</script>
