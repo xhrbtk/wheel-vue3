9c4c623b79160b2e397754d808baa6b4ed2067f4
xhrbtk <xhtbtk@gmail.com>
2023-11-27 13:51:16 +0800

    fix:修改图标

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./index.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
-import './assets/svg.js'
+import './assets/iconfont.js'
 import 'github-markdown-css'
 import Markdown from './components/Markdown.vue';
 
